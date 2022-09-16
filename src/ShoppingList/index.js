5d7c4ebaf064e4c961d5ed54fe3c9f9c8eacf8c6
Michael Cox <ldsmike88@users.noreply.github.com>
2022-09-16 01:20:42 -0400

    Feat: Add Shopping List page placeholder

---
+++ src/ShoppingList/index.js
@@ -0,0 +1,9 @@
+import { Box } from '@mui/material';
+
+const ShoppingList = () => (
+  <Box sx={{ flexDirection: 'row' }}>
+    This is where the shopping list goes.
+  </Box>
+);
+
+export default ShoppingList;

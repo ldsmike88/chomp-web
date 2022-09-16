fd2816d2ab3666894eb0e47aea47c4040d49772b
Michael Cox <ldsmike88@users.noreply.github.com>
2022-09-16 01:11:01 -0400

    Feat: Add MUI theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { CssBaseline } from '@mui/material';
-import { StyledEngineProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Layout from './Layout';
 
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#00344B',
+    },
+    secondary: {
+      main: '#4AA390',
+    },
+  },
+});
+
 const App = () => (
-  <StyledEngineProvider injectFirst>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <Layout />
-  </StyledEngineProvider>
+  </ThemeProvider>
 );
 
 export default App;

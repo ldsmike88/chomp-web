705b2941c5075bd0e660dd9c6da5d63da02f8f3a
Michael Cox <ldsmike88@users.noreply.github.com>
2022-09-17 00:40:27 -0400

    Chore: Update to React 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = document.querySelector('#root');
-ReactDOM.render(<App />, root);
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(<App />);
